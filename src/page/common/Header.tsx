a221b5f2ca7f7e90d4f78d9afd937c3dc4bc3bbc
gmentzik <65217733+gmentzik@users.noreply.github.com>
2021-11-20 12:25:42 +0200

    React Router LInk Test passing url and query params

--- src/page/common/Header.tsx
+++ src/page/common/Header.tsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown, } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link, createSearchParams } from "react-router-dom";
 import logo from '../../images/logo.svg';
 
 // Magic of interconnecting React-Router 5 Link and React-Bootstrap comes from this link:
 // https://stackoverflow.com/questions/54843302/reactjs-bootstrap-navbar-and-routing-not-working-together
+
+const params = { sort: 'date', order: 'newest', myparam: 'myparamvalue' };
+const location =
+{
+    pathname: '/customers/form/25',
+    search: `?${createSearchParams(params)}`,
+}
+
 
 const Header = () => {
     return (
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                        <Navbar.Brand as={Link} to="/">
-                            <img
-                                alt=""
-                                src={logo}
-                                width="30"
-                                height="30"
-                                className="d-inline-block align-top"
-                            />{' '}
-                            Pet Clinic
-                        </Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Pet Clinic
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                             <NavDropdown title="ΠΕΛΑΤΕΣ" id="collasible-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="customers"> ΛΙΣΤΑ ΠΕΛΑΤΩΝ </NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="customers/form">ΝΕΟΣ ΠΕΛΑΤΗΣ</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="customers/form/24">ΠΡΟΒΟΛΗ ΠΕΛΑΤΗ</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="customers/form/24">ΠΡΟΒΟΛΗ ΠΕΛΑΤΗ 24</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to={location}>ΠΡΟΒΟΛΗ ΠΕΛΑΤΗ 25</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Nav>
--- src/page/customer/CustomerForm.tsx
+++ src/page/customer/CustomerForm.tsx
@@ -1,26 +1,42 @@
 import React from "react"
 import { Row } from "react-bootstrap"
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams, useLocation } from "react-router-dom";
 
 const CustomerForm = () => {
 
     const { customerId } = useParams();
+    const searchParams = useSearchParams();
 
     console.log(customerId);
+    console.log(searchParams);
+    console.log(useLocation());
+    const useLocationSearch:string = useLocation().search;
+
+    if (useLocationSearch && useLocationSearch.length > 0) {
+        console.log("Query string exists: " + useLocationSearch);
+        // const queryString = useLocationSearch.substring(2);
+        // console.log(queryString);
+        const query = new URLSearchParams(useLocationSearch);
+        console.log(query.get('sort'));
+        console.log(query.get('order'));
+        console.log(query.get('myparam'));
+        // console.log( query.get('myParam') );
+    }
+
 
     return (
-    <>
-    <Row>
-        <div className="d-flex justify-content-center">
-          {!customerId ? <h1>CREATE NEW CUSTOMER</h1> : <h1>VIEW/EDIT CUSTOMER</h1>}  
-        </div>
-    </Row>
-    <Row>
-        <div className="d-flex justify-content-center">
-        {!customerId ? <h1>NEW CUSTOMER</h1> : <h1>CUSTOMER ID: {customerId} </h1>}
-        </div>
-    </Row>
-    </>
+        <>
+            <Row>
+                <div className="d-flex justify-content-center">
+                    {!customerId ? <h1>CREATE NEW CUSTOMER</h1> : <h1>VIEW/EDIT CUSTOMER</h1>}
+                </div>
+            </Row>
+            <Row>
+                <div className="d-flex justify-content-center">
+                    {!customerId ? <h1>NEW CUSTOMER</h1> : <h1>CUSTOMER ID: {customerId} </h1>}
+                </div>
+            </Row>
+        </>
     );
 }
 
