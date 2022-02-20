7c7a5986c29527e74383ca3dc4b3f2ecf267e777
gmentzik <65217733+gmentzik@users.noreply.github.com>
2022-02-20 21:41:25 +0200

    Created Custom Bootstrap tooltip to abstract overlay

--- src/page/customer/CustomerTableRow.tsx
+++ src/page/customer/CustomerTableRow.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Customer } from "../../api/models/Customer";
+import CustomBSTooltipTop from "../../components/customBSTooltipTop";
 
 interface Props {
     customer: Customer;
@@ -27,29 +27,12 @@ const CustomerTableRow = (props: Props) => {
             <td>{customer.note2}</td>
             <td>{customer.note3}</td>
             <td>
-                <OverlayTrigger
-                    key={`tooltip-${customer.id}-edit-customer`}
-                    placement={'top'}
-                    overlay={
-                        <Tooltip id={`tooltip-${customer.id}-edit-customer`}>
-                            Edit Customer
-                        </Tooltip>
-                    }
-                >
-                    <Link key={customer.id} to={"form/" + customer.id} ><i className="bi bi-pen"></i></Link >
-                </OverlayTrigger>
-                <OverlayTrigger
-                    key={`tooltip-${customer.id}-view-animals`}
-                    placement={'top'}
-                    overlay={
-                        <Tooltip id={`tooltip-${customer.id}-view-animals`}>
-                            View animals
-                        </Tooltip>
-                    }
-                >
-                    <Link style={{paddingLeft: '4px'}} key={customer.id} to={""}><i className="bi bi-clipboard-pulse"></i></Link >
-                </OverlayTrigger>
-
+                <CustomBSTooltipTop tooltipText={"Διόρθωση Στοιχείων"} uniqueIdAndDescriptionText={`${customer.id}-edit-customer`} >
+                    <Link className="paddingLeft4px" key={customer.id} to={"form/" + customer.id} ><i className="bi bi-pen"></i></Link >
+                </CustomBSTooltipTop>
+                <CustomBSTooltipTop tooltipText={"Λίστα κατοικιδίων"} uniqueIdAndDescriptionText={`${customer.id}-view-animals`} >
+                    <Link className="paddingLeft4px" key={customer.id} to={""}><i className="bi bi-clipboard-pulse"></i></Link >
+                </CustomBSTooltipTop>
             </td>
         </tr>
     );
