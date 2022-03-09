8ef17ccf26258910c01f0bb2afac06094f5d980a
gmentzik <65217733+gmentzik@users.noreply.github.com>
2022-03-09 22:43:34 +0200

    Implement Notifications Reducer

--- src/reducers/notificationsReducer.tsx
+++ src/reducers/notificationsReducer.tsx
@@ -1,3 +1,5 @@
+import { NotificationsReducerActionTypes } from "../actions/actionTypes";
+
 export interface Notification {
   index: number;
   type: string;
@@ -12,11 +14,46 @@ const createDefaultState = (): NotificationsState => ({
   notificationsList: [],
 });
 
-export const anotherReducer = (state: NotificationsState = createDefaultState(), action: any): NotificationsState => {
+interface ActionType {
+  type: string;
+  payload: Notification
+}
+
+export const anotherReducer = (state: NotificationsState = createDefaultState(), action: ActionType): NotificationsState => {
   switch (action.type) {
+    case NotificationsReducerActionTypes.ADD_NOTIFICATION:
+      const arrayLength = state.notificationsList.length;
+      const newNotification:Notification = {
+        index: arrayLength,
+        type: action.payload.type,
+        message: action.payload.message
+      }
+      return {
+        ...state,
+        notificationsList: [...state.notificationsList, newNotification] 
+      };
+    case NotificationsReducerActionTypes.REMOVE_NOTIFICATION:
+      return {
+        ...state,
+        notificationsList: [...state.notificationsList, action.payload] 
+      };
+    case NotificationsReducerActionTypes.REMOVE_NOTIFICATION:
+        return {
+          ...state,
+          notificationsList: []
+        };
     default:
       return state;
   }
 };
 
+
+const removeNotificationMessage = (notificationsArray:Notification[], notificationToRemoveIndex:number):Notification[] => {
+  if (notificationsArray.findIndex(
+    (notification) => notification.index === notificationToRemoveIndex
+  )) > -1 {
+    return [];  
+  }
+  return [];
+}
 
